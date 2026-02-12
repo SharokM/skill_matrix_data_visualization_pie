dca5e8a726e7c7df5e30405b7873a872aecae92c
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-02-12 12:35:10 +0000

    hex colors added, font size change

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ pieSlices = [160, 80, 90, 10, 5, 20]
 
 sliceLabels = ["Python", "React.JS", "JavaScript", "NodeJS", "Azure", "Other"]
 
-sliceColors = ["purple", "teal", "yellow", "brown", "blue", "red"]
+sliceColors = ["#8B008B", "#7FFFD4", "#7FFF00", "#D2691E", "#0000FF", "#DC143C"]
 
 plt.pie(pieSlices, labels=sliceLabels, colors=sliceColors)
-plt.title("Programming Language Usage")
+plt.title("Programming Language Usage", fontsize=22)
 plt.savefig("pieSlicePiechart.png")
 plt.show()
+
